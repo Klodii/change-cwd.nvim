9cb44973d04c126e00a8784d9aede21c88669be2
Klodii <k.pepaj@hotmail.it>
2025-01-13 19:28:57 +0100

    feat: created the Telescope picker
    Now I'm working on filling the telescope picker with the right
    directories options.

--- lua/change-cwd.lua
+++ lua/change-cwd.lua
@@ -1,28 +1,71 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local sorters = require "telescope.sorters"
+local actions = require "telescope.actions"
+local action_state = require('telescope.actions.state')
+
 local M = {}
 
---- Select the new working directory
-M.change_working_directory = function()
-  local cwd = vim.fn.getcwd()
-  local buf = vim.api.nvim_create_buf(false, true)       -- Create a new empty buffer
-  vim.api.nvim_buf_set_lines(buf, 0, -1, false, { cwd }) -- Set the message in the buffer
+local options = {
+  root_dir = '~'
+}
 
-  -- Open floating window
-  local current_height = vim.api.nvim_get_option_value('lines', {})
-  local current_width = vim.api.nvim_get_option_value('columns', {})
-  local float_opts = {
-    relative = 'editor', -- The floating window is relative to the editor
-    width = #cwd,
-    height = 1,
-    row = math.floor(current_height * 0.4), -- Window appears near the middle of the screen
-    col = math.floor((current_width - #cwd) * 0.5),
-    style = 'minimal',                      -- Minimal window style with no borders or other decorations
-    border = 'rounded',                     -- Optional: Adds rounded borders
-  }
-
-  -- Open the floating window
-  local win = vim.api.nvim_open_win(buf, true, float_opts) -- Open the window with the buffer content
-
-  vim.api.nvim_buf_set_keymap(buf, 'n', 'q', '<cmd>q<CR>', {})
+M.setup = function(opts)
+  opts = opts or {}
+  opts.root_dir = opts.root_dir or '~'
+  options = opts
 end
 
+
+-- Telescope picker, used to select the new working directory
+-- @param directories: string[] the list of directories
+-- @param callback: function Since the picker is asynchronous and you can't
+--                           directly return from the picker in a synchronous
+--                           function, a common approach is to use a callback
+--                           or store the selected item in a global variable.
+local select_directory = function(directories, callback)
+  pickers.new({
+    prompt_title = "Choose a directory",
+    finder = finders.new_table({
+      results = directories,
+    }),
+    sorter = sorters.get_generic_fuzzy_sorter(),
+    previewer = false,
+    attach_mappings = function(prompt_bufnr, map)
+      -- action to do when selecting an option
+      actions.select_default:replace(function()
+        local selection = action_state.get_selected_entry()
+        if selection then
+          callback(selection[1])
+        end
+        actions.close(prompt_bufnr) -- Close the picker
+      end)
+      return true
+    end,
+  }):find()
+end
+
+--- Change the working directory
+--- @param dir: string new working directory
+local change_working_directory = function(selected_item)
+  vim.notify("You selected: " .. selected_item, vim.log.levels.INFO)
+end
+
+--- Select the new working directory
+M.ask_new_directory = function()
+  local dir_options = vim.fn.system("ls -l")
+  -- vim.v.shell_error: The exit status of the last shell command run with vim.fn.system().
+  -- If the exit code is non-zero, it indicates an error
+  if vim.v.shell_error ~= 0 then
+    vim.notify("Command failed with error: " .. dir_options, vim.log.levels.ERROR)
+    return
+  end
+  print(dir_options)
+
+  select_directory({ 'dir1', 'dir2' }, change_working_directory)
+end
+
+
+-- run locally for testing
+-- M.ask_new_directory()
 return M
