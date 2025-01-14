de8662efa8ac0584ac28e00f0f0a36c5af34cb37
Klodii <k.pepaj@hotmail.it>
2025-01-14 16:09:40 +0100

    feat: add root_dir functionality

--- lua/change-cwd.lua
+++ lua/change-cwd.lua
@@ -4,28 +4,39 @@ local sorters = require "telescope.sorters"
 local actions = require "telescope.actions"
 local action_state = require('telescope.actions.state')
 
+--- Splits a string into a table based on a delimiter
+---@param input_string string example: 'a,b,c'
+---@param delimiter string example: ','
+---@return table example: {'a', 'b', 'c'}
+local split_string = function(input_string, delimiter)
+  delimiter = delimiter or '%s'
+  local splitted_string = {}
+  local i = 1
+  for str in string.gmatch(input_string, '([^' .. delimiter .. ']+)') do
+    splitted_string[i] = str
+    i = i + 1
+  end
+  return splitted_string
+end
+
 local M = {}
 
 local options = {
-  root_dir = '~',
-  explorer = 'Oil'
+  root_dir = '~/',
 }
 
 M.setup = function(opts)
   opts = opts or {}
-  opts.root_dir = opts.root_dir or '~'
-  -- opts.explorer = opts.explorer or 'Explorer'
-  opts.explorer = opts.explorer or 'Explorer'
+  opts.root_dir = opts.root_dir or '~/'
   options = opts
 end
 
-
--- Telescope picker, used to select the new working directory
--- @param directories: string[] the list of directories
--- @param callback: function Since the picker is asynchronous and you can't
---                           directly return from the picker in a synchronous
---                           function, a common approach is to use a callback
---                           or store the selected item in a global variable.
+--- Telescope picker, used to select the new working directory
+--- @param directories string[] the list of directories
+--- @param callback function Since the picker is asynchronous and you can't
+---                           directly return from the picker in a synchronous
+---                           function, a common approach is to use a callback
+---                           or store the selected item in a global variable.
 local select_directory = function(directories, callback)
   pickers.new({
     prompt_title = "Choose a directory",
@@ -45,20 +56,19 @@ local select_directory = function(directories, callback)
       end)
       return true
     end,
-  }):find()
+  }, {}):find()
 end
 
 --- Change the working directory
---- @param dir: string new working directory
-local change_working_directory = function(selected_item)
-  vim.api.nvim_command('cd ' .. selected_item)
-  vim.api.nvim_command(options.explorer)
-  vim.notify("Changed working directory to: " .. selected_item, vim.log.levels.INFO)
+--- @param dir_path string path of the new working directory
+local _change_working_directory = function(dir_path)
+  vim.api.nvim_command('cd ' .. dir_path)
+  vim.notify("Changed working directory to: " .. dir_path, vim.log.levels.INFO)
 end
 
 --- Select the new working directory
-M.ask_new_directory = function()
-  local dir_options = vim.fn.system("ls -l")
+M.change_working_directory = function()
+  local dir_options = vim.fn.system("ls -d " .. options.root_dir .. "*/")
   -- vim.v.shell_error: The exit status of the last shell command run with vim.fn.system().
   -- If the exit code is non-zero, it indicates an error
   if vim.v.shell_error ~= 0 then
@@ -66,10 +76,8 @@ M.ask_new_directory = function()
     return
   end
 
-  select_directory({ '~/.config/nvim', '~/playground/nvim-plugins/change-cwd.nvim' }, change_working_directory)
+  local dir_options_table = split_string(dir_options, '\n')
+  select_directory(dir_options_table, _change_working_directory)
 end
 
-
--- run locally for testing
--- M.ask_new_directory()
 return M
